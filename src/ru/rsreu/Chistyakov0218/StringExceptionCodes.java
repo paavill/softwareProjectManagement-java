37ed4a0117969446158bf4f68389fd63bb4b5271
paavill <pavelchist2001@gmail.com>
2021-03-23 21:22:55 +0300

    Final version of lab2

---
+++ src/ru/rsreu/Chistyakov0218/StringExceptionCodes.java
@@ -0,0 +1,11 @@
+package ru.rsreu.Chistyakov0218;
+
+/**
+ * Enumerating error codes for wrong string state
+ * 
+ * @author Chistyakov Pavel
+ *
+ */
+public enum StringExceptionCodes {
+	EMPTY, NO_SPACES
+}
